5c2f9859c810fdfeb35f0f0c881b317d9cf04ea5
Djalma <djalma.ant@gmail.com>
2021-12-08 13:06:07 -0300

    Predicate implementation

---
+++ src/application/Program.java
@@ -0,0 +1,28 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.Product;
+import util.ProductPredicate;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		List<Product> list = new ArrayList<>();
+		
+		list.add(new Product("Playstation 5", 5999.99));
+		list.add(new Product("Iphone 11", 4999.90));
+		list.add(new Product("TV Lg", 3999.99));
+		list.add(new Product("The Sims 4", 99.90));
+		list.add(new Product("Mafia III", 48.50));
+		list.add(new Product("Xcom 2", 9.99));
+		
+		list.removeIf(new ProductPredicate());
+		
+		list.forEach(System.out::println);
+
+	}
+
+}
